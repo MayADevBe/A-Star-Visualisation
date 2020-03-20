4a393adbe976e04b109b41fef770e75342815cb8
MayADevBe <o3wl.codes@gmail.com>
2020-03-20 21:35:40 +0100

    Custom branch
    Still problem with visualisation

--- create_map.py
+++ create_map.py
@@ -4,13 +4,17 @@ import random
 
 class MapCreation:
 
-    def __init__(self, R_C, W_H):
+    def __init__(self, R_C, W_H, board):
         self.walls = []
         self.start = None
         self.goal = None
         self.R_C = R_C
         self.W_H = W_H
         self.ready = False
+        self.board = board
+        board.platform.bind("<Button-1>", self.custom_map)
+        board.platform.bind("<Return>", self.start_search)
+        board.platform.focus_set()
 
     def random_map(self):
         self.start = (random.randint(0, self.R_C-1), random.randint(0, self.R_C-1))
@@ -22,41 +26,43 @@ class MapCreation:
                     self.walls.append(wall)
 
     def custom_map(self, event):
-        item = board.platform.find_closest(event.x, event.y)[0]
-        x = floor(event.x/W_H)
-        y = floor(event.y/W_H)
+        if not self.ready:
+            item = self.board.platform.find_closest(event.x, event.y)[0]
+            x = floor(event.x/self.W_H)
+            y = floor(event.y/self.W_H)
 
-        if self.start == None:
-            self.start = (x, y)
-            board.platform.itemconfig(item, fill='blue')
-        elif self.goal == None:
-            self.goal = (x, y)
-            board.platform.itemconfig(item, fill='blue')
-        elif self.start == (x,y):
-            self.start = None
-            board.platform.itemconfig(item, fill='white')
-        elif self.goal == (x,y):
-            self.goal = None
-            board.platform.itemconfig(item, fill='white')
-        elif (x, y) in self.walls:
-            board.platform.itemconfig(item, fill='white')
-            self.walls.remove((x, y))
-        else:
-            board.platform.itemconfig(item, fill='black')
-            self.walls.append((x, y))
-        board.platform.update()
+            if self.start == None:
+                self.start = (x, y)
+                self.board.platform.itemconfig(item, fill='blue')
+            elif self.goal == None:
+                self.goal = (x, y)
+                self.board.platform.itemconfig(item, fill='blue')
+            elif self.start == (x,y):
+                self.start = None
+                self.board.platform.itemconfig(item, fill='white')
+            elif self.goal == (x,y):
+                self.goal = None
+                self.board.platform.itemconfig(item, fill='white')
+            elif (x, y) in self.walls:
+                self.board.platform.itemconfig(item, fill='white')
+                self.walls.remove((x, y))
+            else:
+                self.board.platform.itemconfig(item, fill='black')
+                self.walls.append((x, y))
+            self.board.platform.update()
 
     def start_search(self, event=None):
-        if self.start == None or self.goal == None:
-            self.random_map()
-        self.ready = True
+        if not self.ready:
+            if self.start == None or self.goal == None:
+                self.random_map()
+            self.ready = True
 
-R_C = 25 #Rows and Columns
-W_H = 20 #Width and Height
-map_creation = MapCreation(R_C, W_H)
-board = Board("A*-Algorithm", map_creation.R_C, map_creation.R_C, map_creation.W_H)
-board.platform.bind("<Button-1>", map_creation.custom_map)
-board.platform.bind("<Return>", MapCreation.start_search)
-board.platform.focus_set()
-board.draw()
-board.start()
+# R_C = 25 #Rows and Columns
+# W_H = 20 #Width and Height
+# map_creation = MapCreation(R_C, W_H)
+# board = Board("A*-Algorithm", map_creation.R_C, map_creation.R_C, map_creation.W_H)
+# board.platform.bind("<Button-1>", self.custom_map)
+# board.platform.bind("<Return>", MapCreation.start_search)
+# board.platform.focus_set()
+# board.draw()
+# board.start()
--- main.py
+++ main.py
@@ -2,22 +2,36 @@ import threading
 import time
 from board import Board
 from a_star import AStar
+from create_map import MapCreation
 
 R_C = 25 #Rows and Columns
 W_H = 20 #Width and Height
-start = (0,0)
-goal = (24,24)
-walls = []
 
 cost = None
 
-def starThread():
+def star_thread():
     global a_star, cost#, path
     cost = a_star.search()
 
+def creation_thread():
+    global a_star, walls, goal, start, map_creation
+    while not map_creation.ready:
+        time.sleep(1)
+
+    start = map_creation.start
+    goal = map_creation.goal
+    walls = map_creation.walls
+
+    a_star = AStar(start, goal, R_C, R_C, walls)
+
+    t2 = threading.Thread(target=star_thread)
+    t2.daemon = True
+    t2.start() 
+    draw_path()   
+
 def draw_path():
     global board, a_star, cost, walls
-    board.draw()
+    #board.draw()
     while cost == None:
         board.draw_coordinates([a_tuple[1] for a_tuple in a_star.openlist.queue], "green")
         board.draw_coordinates(a_star.closedlist, "yellow")
@@ -30,7 +44,6 @@ def draw_path():
         time.sleep(0.01)
 
     board.draw()
-    #board.draw_coordinates([a_tuple[1] for a_tuple in a_star.openlist.queue], "green")
     board.draw_coordinates(a_star.closedlist, "yellow")
     board.draw_coordinates(walls, "black")
     if cost == -1:
@@ -42,13 +55,14 @@ def draw_path():
     board.platform.update()
     time.sleep(0.01)
 
-board = Board("A*-Algorithm", R_C, R_C, W_H )
 
-a_star = AStar(start, goal, R_C, R_C, walls)
+board = Board("A*-Algorithm", R_C, R_C, W_H)
+map_creation = MapCreation(R_C, W_H, board)
+board.draw()
 
-t2 = threading.Thread(target=starThread)
-t2.daemon = True
-t2.start()
-draw_path()
+t = threading.Thread(target=creation_thread)
+t.daemon = True
+t.start()
 board.start()
+
 
