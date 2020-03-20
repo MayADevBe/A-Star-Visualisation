2728a3406abba081c05870420578920093561cf5
MayADevBe <o3wl.codes@gmail.com>
2020-03-20 19:33:43 +0100

    Random walls, start, goal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import random
 from board import Board
 from a_star import AStar
 
@@ -10,42 +11,53 @@ goal = (24,24)
 walls = []
 
 cost = None
-#path = []
 
-board = Board("A*-Algorithm", R_C, R_C, W_H )
-
-a_star = AStar(start, goal, R_C, R_C, walls)
+def random_search():
+    global start, end, walls
+    start = (random.randint(0, R_C-1), random.randint(0, R_C-1))
+    end = (random.randint(0, R_C-1), random.randint(0, R_C-1))
+    for i in range(random.randint(0, 500)):
+        wall = (random.randint(0, R_C-1), random.randint(0, R_C-1))
+        if not wall == start and not wall == goal:
+            if not wall in walls:
+                walls.append(wall)
 
 def starThread():
-    print("A* Thread started")
     global a_star, cost#, path
     cost = a_star.search()
 
 def draw_path():
-    #global a_star, board
-    global board, a_star, cost
+    global board, a_star, cost, walls
     board.draw()
     while cost == None:
-        print("Drawing Thread started")
         board.draw_coordinates([a_tuple[1] for a_tuple in a_star.openlist.queue], "green")
-        board.draw_coordinates(a_star.closedlist, "red")
-        board.draw_coordinates(a_star.path, "black")
+        board.draw_coordinates(a_star.closedlist, "yellow")
+        board.draw_coordinates(a_star.path, "orange")
         
-        board.draw_coordinates([start, goal], "yellow")
+        board.draw_coordinates(walls, "black")
+        board.draw_coordinates([start, goal], "blue")
 
         board.platform.update()
         time.sleep(0.01)
 
     board.draw_coordinates([a_tuple[1] for a_tuple in a_star.openlist.queue], "green")
-    board.draw_coordinates(a_star.closedlist, "red")
+    board.draw_coordinates(a_star.closedlist, "yellow")
+    board.draw_coordinates(walls, "black")
     if cost == -1:
-        board.draw_coordinates(a_star.path, "orange")
-        board.draw_coordinates([start, goal], "yellow")
+        board.draw_coordinates(a_star.path, "red")
+        board.draw_coordinates([start, goal], "blue")
+        print("Couldn't find path")
     else:
-        board.draw_coordinates(a_star.path, "black")
+        board.draw_coordinates(a_star.path, "blue")
+        print(f"Path: {a_star.path}")
 
     board.platform.update()
     time.sleep(0.01)
+
+board = Board("A*-Algorithm", R_C, R_C, W_H )
+
+random_search()
+a_star = AStar(start, goal, R_C, R_C, walls)
 
 t2 = threading.Thread(target=starThread)
 t2.daemon = True
